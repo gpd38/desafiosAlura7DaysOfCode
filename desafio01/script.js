bce6c4ccd717f4f30e5584e56800e6feee576a41
Gustavo Dias <gustavodias2@gmail.com>
2022-03-29 08:27:00 -0300

    desafio01

---
+++ desafio01/script.js
@@ -0,0 +1,60 @@
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+if (numeroUm == stringUm) {
+    console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes');
+    } else {
+    console.log('As variáveis numeroUm e stringUm não tem o mesmo valor');
+  }
+
+ if (numeroTrinta === stringTrinta) {
+   console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+   } else {
+   console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+  }
+
+  if (numeroDez == stringDez) {
+   console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+   } else {
+   console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+  }
+
+
+
+// if (numeroUm > stringUm) {
+//    console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes');
+//    } else {
+//    console.log('As variáveis numeroUm e stringUm não tem o mesmo valor');
+//   } 
+
+ // // if (numeroTrinta !== stringTrinta) {
+ // //   console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+ // //   } else {
+ // //   console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+ // //  }
+
+ // if (numeroDez === stringDez) {
+ //   console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+ //   } else {
+ //   console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+ //  }
+
+
+
+// 
+// console.log(1 != '1') //retorna false
+// console.log(10 != '10') //retorna false
+// console.log(30 !== '30') //retorna true
+// console.log(1 !== '1') //retorna true
+// console.log(10 !== 10) //retorna false
+// console.log(30 !== 30) //retorna false
+
+// console.log(true === 'true') //retorna false
+// console.log(10 === '10') //retorna false
+// console.log(true === true) //retorna true
+// console.log(10 === 10) //retorna true
+
